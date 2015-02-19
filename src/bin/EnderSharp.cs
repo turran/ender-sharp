07b0ed8bb7648a5ac16c8d4392d12583e791bee3
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-02-19 01:58:36 +0100

    Generate the objects

--- src/bin/EnderSharp.cs
+++ src/bin/EnderSharp.cs
@@ -1,4 +1,8 @@
 using Ender;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using Microsoft.CSharp;
 
 public class EnderSharp
 {
@@ -6,10 +10,37 @@ public class EnderSharp
 	{
 	}
 
+	public static string generateCSharpCode(CodeCompileUnit cu, string outfile)
+	{
+		// Generate the code with the C# code provider.
+		CSharpCodeProvider provider = new CSharpCodeProvider();
+
+		// Build the output file name. 
+		string sourceFile;
+		if (provider.FileExtension[0] == '.')
+		{
+			sourceFile = outfile + provider.FileExtension;
+		}
+		else
+		{
+			sourceFile = outfile + "." + provider.FileExtension;
+		}
+
+		StreamWriter sw = new StreamWriter(sourceFile, false);
+		IndentedTextWriter tw = new IndentedTextWriter(sw, "    ");
+		provider.GenerateCodeFromCompileUnit(cu, tw,
+				new CodeGeneratorOptions());
+		tw.Close();
+
+		return sourceFile;
+	}
+
+
 	public static void Main(string[] args)
 	{
-		/* do the args processing */
+		/* TODO do the args processing */
 		Generator eg = new Generator();
-		eg.generate(args[0]);
+		CodeCompileUnit cu = eg.generate(args[0]);
+		generateCSharpCode(cu, args[0]);
 	}
 }
--- src/lib/Generator.cs
+++ src/lib/Generator.cs
@@ -19,6 +19,19 @@ namespace Ender
 		DEF,
 	}
 
+	public enum Case
+	{
+		CAMEL,
+		PASCAL,
+		UNDERSCORE,
+	}
+
+	public enum Notation
+	{
+		ENGLISH,
+		LATIN,
+	}
+
 	public class Generator
 	{
 		/* ender_main.h */
@@ -32,22 +45,63 @@ namespace Ender
 		[DllImport("libender.dll")]
 		static extern IntPtr ender_lib_item_list(IntPtr lib, ItemType type);
 		//static extern int ender_lib_version_get(const Ender_Lib *thiz);
-		//static extern const char * ender_lib_name_get(const Ender_Lib *thiz);
-		//static extern Eina_List * ender_lib_dependencies_get(const Ender_Lib *thiz);
+		[DllImport("libender.dll")]
+		static extern string ender_lib_name_get(IntPtr lib);
+		[DllImport("libender.dll")]
+		static extern IntPtr ender_lib_dependencies_get(IntPtr lib);
 		//static extern Ender_Item * ender_lib_item_find(const Ender_Lib *thiz, const char *name);
 		/* ender_item.h */
 		//static extern Ender_Item * ender_item_ref(Ender_Item *thiz);
 		//static extern void ender_item_unref(Ender_Item *thiz);
-		//static extern const char * ender_item_name_get(Ender_Item *thiz);
+		[DllImport("libender.dll")]
+		static extern string ender_item_name_get(IntPtr i);
 		//static extern Ender_Item_Type ender_item_type_get(Ender_Item *thiz);
 		//static extern Ender_Item * ender_item_parent_get(Ender_Item *thiz);
 		//static extern const char * ender_item_type_name_get(Ender_Item_Type type);
 		//static extern const Ender_Lib * ender_item_lib_get(Ender_Item *thiz);
 		//static extern Eina_Bool ender_item_is_exception(Ender_Item *i);
+		/* ender_utils.h */
+		[DllImport("libender.dll")]
+		static extern string ender_utils_name_convert(string s, Case scase, Notation snot,
+				Case dcase, Notation dnot);
 
-		private void generateObject(IntPtr item)
+		private void generateObject(CodeNamespace ns, IntPtr item)
 		{
+			/*
+			String name = ender_utils_name_convert(
+					ender_item_name_get(item),
+					Case.UNDERSCORE,
+					Notation.LATIN, Case.CAMEL,
+					Notation.ENGLISH);
+			*/
+			String name = ender_item_name_get(item);
+			CodeTypeDeclaration o = new CodeTypeDeclaration(name);
+			ns.Types.Add(o);
+		}
 
+		private CodeNamespace generateLib(CodeCompileUnit cu, IntPtr lib)
+		{
+			CodeNamespace root = new CodeNamespace();
+			cu.Namespaces.Add(root);
+			/* add the imports/using */
+			List list = new List(ender_lib_dependencies_get(lib));
+			foreach (IntPtr l in list)
+			{
+				/*
+				String name = ender_utils_name_convert(
+						ender_lib_name_get(l),
+						Case.UNDERSCORE,
+						Notation.LATIN, Case.CAMEL,
+						Notation.ENGLISH);
+				*/
+				String name = ender_lib_name_get(l);
+				root.Imports.Add(new CodeNamespaceImport(name));
+			}
+			/* TODO add a method on ender to free the dependency list? */
+			/* add the new namespace */
+			CodeNamespace ns = new CodeNamespace(ender_lib_name_get(lib));
+			cu.Namespaces.Add(ns);
+			return ns;
 		}
 
 		private void generateItem(IntPtr item)
@@ -55,31 +109,28 @@ namespace Ender
 
 		}
 
-		private void generateLib(IntPtr lib)
+		public CodeCompileUnit generate(string nsName)
 		{
-
-		}
-
-		public CodeCompileUnit generate(string ns)
-		{
-			List list;
-			IntPtr lib;
-
 			/* get the ender library */
-			lib = ender_lib_find(ns);
+			IntPtr lib = ender_lib_find(nsName);
 			if (lib == IntPtr.Zero)
 				return null;
-			list = new List(ender_lib_item_list(lib, ItemType.OBJECT));
+			CodeCompileUnit cu = new CodeCompileUnit();
+			/* first set the information from the library itself */
+			CodeNamespace ns = generateLib(cu, lib);
+
+			List list = new List(ender_lib_item_list(lib, ItemType.OBJECT));
 			foreach (IntPtr item in list)
 			{
-				System.Console.WriteLine("there");
+				generateObject(ns, item);
 			}
+			/* TODO add a method on ender to free an item based list */
 			/* iterate over every object */
 			/* jump to root nodes, ie. items that do not have inheritances */
 			/* in the mean time add every non root node into a queue */
 			/* iterate the childs and remove theme from the queue in case it is already processed */
 			/* finally continue with the queue again */
-			return null;
+			return cu;
 		}
 
 		/* initialize ender before we do anything */
