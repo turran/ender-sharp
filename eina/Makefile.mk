43095a8b723723d882f8a5d07f1e67e577e4da68
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-04-10 21:23:28 +0200

    Add the compilation of eina and enesim

--- eina/Makefile.mk
+++ eina/Makefile.mk
@@ -4,3 +4,12 @@ EXTRA_DIST += eina/eina-sharp.cs
 
 eina-sharp.cs:
 	$(output_DIR)/ender2sharp.exe -o $(top_srcdir)/eina eina
+
+eina_BIN = eina-sharp.dll
+eina_SRC = $(top_srcdir)/eina/eina-sharp.cs
+
+noinst_DATA += $(eina_BIN)
+
+$(eina_BIN): $(eina_SRC) $(output_DIR)
+	$(CSC) -nowarn:169 -unsafe -target:library -debug $(eina_SRC) \
+		-out:$(output_DIR)/$(eina_BIN)
--- enesim/Makefile.mk
+++ enesim/Makefile.mk
@@ -5,3 +5,11 @@ EXTRA_DIST += enesim/enesim-sharp.cs
 enesim-sharp.cs:
 	$(output_DIR)/ender2sharp.exe -o $(top_srcdir)/enesim enesim
 
+enesim_BIN = enesim-sharp.dll
+enesim_SRC = $(top_srcdir)/enesim/enesim-sharp.cs
+
+noinst_DATA += $(enesim_BIN)
+
+$(enesim_BIN): $(eina_BIN) $(enesim_SRC) $(output_DIR)
+	$(CSC) -nowarn:169 -unsafe -target:library -debug $(enesim_SRC) \
+		-out:$(output_DIR)/$(enesim_BIN)
