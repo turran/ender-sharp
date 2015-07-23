2754f5dbd1797aff64c3df5523ee2924e1ec0ed1
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-23 21:26:37 +0200

    Skip more unsupported features
    Basically the interfaces to create external modules are not
    supported on the bindings yet. There is a lib that supports
    a DllExport annotation for C <-> C# interop, but that means
    a lot of work to do.

--- enesim/Makefile.mk
+++ enesim/Makefile.mk
@@ -3,7 +3,9 @@ pkgconfig_DATA += enesim/enesim-sharp.pc
 EXTRA_DIST += enesim/enesim-sharp.cs
 
 enesim-sharp.cs:
-	$(output_DIR)/ender2sharp.exe -s enesim.log.add_parametric -o $(top_srcdir)/enesim enesim
+	$(output_DIR)/ender2sharp.exe -s enesim.log.add_parametric \
+	-s enesim.image.provider -s enesim.image.finder -s enesim.image.provider.descriptor \
+	-s enesim.image.finder.descriptor -o $(top_srcdir)/enesim enesim
 
 enesim_BIN = enesim-sharp.dll
 enesim_SRC = $(top_srcdir)/enesim/enesim-sharp.cs
