69f2d94c83cd8a97b6c8f6147aedcf56ae8de152
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-26 17:53:26 +0200

    Sign the ender assembly

--- src/lib/Makefile.mk
+++ src/lib/Makefile.mk
@@ -19,7 +19,7 @@ $(top_srcdir)/src/lib/Generator.cs
 
 $(output_DIR)/$(lib_BIN): $(lib_SRC) $(output_CHECK)
 	$(CSC) -nowarn:169 -unsafe -target:library -debug $(lib_SRC) \
-		-out:$(output_DIR)/$(lib_BIN)
+		-out:$(output_DIR)/$(lib_BIN) -keyfile:$(top_srcdir)/ender-sharp.snk
 
 enderlibdir = $(libdir)/ender-sharp
 enderlib_DATA = $(output_DIR)/$(lib_BIN)
