e764f60af362389edf534b4033b3830446a99bdc
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-31 11:02:32 +0200

    Add more skipped items

--- egueb-dom/Makefile.mk
+++ egueb-dom/Makefile.mk
@@ -6,8 +6,8 @@ egueb-dom-sharp.cs:
 	$(output_DIR)/ender2sharp.exe -s egueb.dom.attr.get -s egueb.dom.attr.set \
 	-s egueb.dom.attr.final_get -s egueb.dom.attr.final_get_va \
 	-s egueb.dom.element.attribute_masked_set -s egueb.dom.element.attribute_masked_get \
-	-s egueb.dom.string.new_with_static_string -s egueb.dom.string.steal \
-	-o $(top_srcdir)/egueb-dom egueb-dom
+	-s egueb.dom.string.new_with_static_string -s egueb.dom.string.string_steal \
+	-s egueb.dom.string.steal -o $(top_srcdir)/egueb-dom egueb-dom
 
 egueb_dom_BIN = egueb-dom-sharp.dll
 egueb_dom_SRC = $(top_srcdir)/egueb-dom/egueb-dom-sharp.cs
