e249f1406d2a29557b0245c2a4223822f5d491e7
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-08-02 14:00:02 +0200

    Add the glue for the constants

--- egueb-dom/egueb-dom-sharp-glue.c
+++ egueb-dom/egueb-dom-sharp-glue.c
@@ -1,1 +1,16 @@
-#include "Egueb_Dom.h"
+#include "egueb-dom-sharp-glue.h"
+
+EAPI Egueb_Dom_String * egueb_dom_window_feature_window_name_get(void)
+{
+	return egueb_dom_string_ref(EGUEB_DOM_WINDOW_FEATURE_WINDOW_NAME_GET);
+}
+
+EAPI Egueb_Dom_String * egueb_dom_event_key_down_get(void)
+{
+	return egueb_dom_string_ref(EGUEB_DOM_EVENT_KEY_DOWN);
+}
+
+EAPI Egueb_Dom_String * egueb_dom_event_key_up_get(void)
+{
+	return egueb_dom_string_ref(EGUEB_DOM_EVENT_KEY_DOWN);
+}
---
+++ egueb-dom/egueb-dom-sharp-glue.h
@@ -0,0 +1,35 @@
+#ifndef _EGUEB_DOM_GLUE_H
+#define _EGUEB_DOM_GLUE_H
+
+#include "Egueb_Dom.h"
+
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EGUEB_DOM_GLUE_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI __declspec(dllimport)
+# endif
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif
+
+EAPI Egueb_Dom_String * egueb_dom_window_feature_window_name_get(void);
+EAPI Egueb_Dom_String * egueb_dom_event_key_down_get(void);
+EAPI Egueb_Dom_String * egueb_dom_event_key_up_get(void);
+
