bfe84bd63afbe5e0806ff440c15a7d472f4a7b33
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-09-18 20:07:22 +0200

    Make sure to install egueb related bindings
    Also include the dll.config on the gac installation

--- egueb-dom/Makefile.mk
+++ egueb-dom/Makefile.mk
@@ -12,14 +12,18 @@ egueb-dom-sharp.cs:
 
 egueb_dom_BIN = egueb-dom-sharp.dll
 egueb_dom_SRC = $(top_srcdir)/egueb-dom/egueb-dom-sharp.cs
+egueb_dom_DLLCONFIG = $(egueb_dom_BIN).config
 
 egueb_domdllconfigdir = $(libdir)/egueb-dom-sharp
-egueb_domdllconfig_DATA = egueb-dom/egueb-dom-sharp.dll.config
+egueb_domdllconfig_DATA = egueb-dom/$(egueb_dom_DLLCONFIG)
 
 egueb_domlibdir = $(libdir)/egueb-dom-sharp
 egueb_domlib_DATA = $(output_DIR)/$(egueb_dom_BIN)
 
-$(output_DIR)/$(egueb_dom_BIN): $(output_DIR)/$(lib_BIN) $(output_DIR)/$(enesim_BIN) $(output_DIR)/$(eina_BIN) $(egueb_dom_SRC) $(output_CHECK)
+$(output_DIR)/$(egueb_dom_DLLCONFIG): $(egueb_domdllconfig_DATA) $(output_CHECK)
+	@cp $< $@
+
+$(output_DIR)/$(egueb_dom_BIN): $(output_DIR)/$(lib_BIN) $(output_DIR)/$(enesim_BIN) $(output_DIR)/$(eina_BIN) $(output_DIR)/$(egueb_dom_DLLCONFIG) $(egueb_dom_SRC) $(output_CHECK)
 	$(CSC) -nowarn:169 -unsafe -target:library -debug $(egueb_dom_SRC) \
 		-r:$(output_DIR)/eina-sharp.dll -r:$(output_DIR)/enesim-sharp.dll -r:$(output_DIR)/ender-sharp.dll \
 		-out:$(output_DIR)/$(egueb_dom_BIN) -keyfile:$(top_srcdir)/egueb-dom/egueb-dom-sharp.snk
